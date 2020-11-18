6d1319c3008261ca355044cedba7e04501764095
Konnosx17 <73156884+Konnosx17@users.noreply.github.com>
2020-11-18 10:40:35 +0200

    Update git1.py

--- git1.py
+++ git1.py
@@ -1,6 +1,6 @@
 itm=raw_input("Item:")
 price=input("Price:")
 discount=(0.2 * price)
-#0.2 is the decimal conversion for a 20% discount and can be easily changed
+#0.2 is the conversion for a 20% discount and can be easily changed by the divison of the discount with 100
 total=price-discount
 print "This", itm ,"cost",int(total),"from",price
